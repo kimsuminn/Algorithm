76d7371ae025eb574a0d6f0ce53c62ee920b5f80
Sumin <suminkim0618@gmail.com>
2023-03-12 14:43:55 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.py
@@ -0,0 +1,9 @@
+def solution(age):
+    age_dict = {
+        '0': 'a', '1': 'b', '2': 'c', '3': 'd', '4': 'e',
+        '5': 'f', '6': 'g', '7': 'h', '8': 'i', '9': 'j'
+    }
+    answer = ''
+    for i in str(age):
+        answer += age_dict[i]
+    return answer
